a8cceed8dca5ef8dfe1fe047f483efbeeb689a90
hafzansh <hafizansh.m@gmail.com>
2020-04-27 05:45:24 +0800

    menambahkan beberapa array untuk kasus perhitungan nilai

---
+++ hitungNilai.java
@@ -0,0 +1,9 @@
+
+public class hitungNilai {
+    public static void main(String[] args){
+        String nama[] = {"Hafiz","Nisha","Hanif","Rizky","Jinny"};
+        int nilai1[] = {80,75,70,65,40},nilai2[] = {95,75,60,70,50};
+        double[] rata = new double[5];
+        
+    }
+}
