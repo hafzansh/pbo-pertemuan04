1b0361911be047490215a78e6500fb1eb7859960
hafzansh <hafizansh.m@gmail.com>
2020-04-27 05:45:51 +0800

    add output untuk tampilan header

--- hitungNilai.java
+++ hitungNilai.java
@@ -5,5 +5,9 @@ public class hitungNilai {
         int nilai1[] = {80,75,70,65,40},nilai2[] = {95,75,60,70,50};
         double[] rata = new double[5];
         
+        System.out.println("------------------------------------------------------------");
+        System.out.println("| No. |         Nama         | Test 1 | Test 2 | Rata-rata |");
+        System.out.println("------------------------------------------------------------");
+        
     }
 }
