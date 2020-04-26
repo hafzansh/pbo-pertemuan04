b28ab6b50509130646aa75c13d131339b88a1297
hafzansh <hafizansh.m@gmail.com>
2020-04-27 05:09:23 +0800

    perulangan dengan menggunakan while

---
+++ loop.java
@@ -0,0 +1,18 @@
+
+public class loop {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+        
+        System.out.println("Isi gelas sekarang :" + isiGelas + "ml");
+        System.out.println("Isi gelas keitka penuh : " + penuh + "ml");
+
+        while(isiGelas != penuh){
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("Isi gelas sekarang : " + isiGelas + "ml");            
+        }
+        
+        System.out.println("Finale, Isi gelas sekarang : " + isiGelas + "ml");
+    }
+}
