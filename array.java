3e46af3e75c4ddb0d957c75a51b0eaaec28c9dff
hafzansh <hafizansh.m@gmail.com>
2020-04-27 05:07:49 +0800

    penggunaan multidimensional array pada perhitungan nilai mahasiswa

--- array.java
+++ array.java
@@ -1,12 +1,36 @@
 public class array {
     public static void main(String[] args){
-        String mataKuliah[];
-        mataKuliah = new String[10];
+        double bobot[] = {4,3.7,3.3,3};
+        double[][] nilaiMahasiswa = new double[5][30];
 
-        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
-        mataKuliah[1] = "Visual 2";
-        mataKuliah[2] = "Rekayasa Perangkat Lunak";
-        mataKuliah[3] = "Organisasi Komputer";
-        mataKuliah[4] = "Pemrograman Web 2";
+        nilaiMahasiswa[0][0] = 80;
+        nilaiMahasiswa[1][0] = 90;
+        nilaiMahasiswa[2][0] = 75;
+        nilaiMahasiswa[3][0] = 70;
+
+        nilaiMahasiswa[4][0] = nilaiMahasiswa[0][0]  * bobot [0] +
+            nilaiMahasiswa[1][0] * bobot[1] +
+            nilaiMahasiswa[2][0] * bobot[2] +
+            nilaiMahasiswa[3][0] * bobot[3];
+
+        System.out.println("Nilai si mahasiswa 1 : " + nilaiMahasiswa[4][0]);   
+
+        nilaiMahasiswa[0][1] = 80;
+        nilaiMahasiswa[1][1] = 90;
+        nilaiMahasiswa[2][1] = 75;
+        nilaiMahasiswa[3][1] = 70;
+
+        nilaiMahasiswa[4][1] = 0;
+        for (int i=0; i<=3; i++) {
+            nilaiMahasiswa[4][1] += nilaiMahasiswa[i][1] * bobot[1];
+        }
+
+        System.out.println("Nilai si mahasiswa 2 " + nilaiMahasiswa[4][1]);
+
+        double jumlahNilaiAkhir = 0;
+        for (int i=0; i<=3; i++) {
+            jumlahNilaiAkhir += nilaiMahasiswa[4][i];
+        }
+        System.out.println(jumlahNilaiAkhir);
     }
 }
